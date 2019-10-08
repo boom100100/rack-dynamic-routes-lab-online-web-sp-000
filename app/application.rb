496cedf9057dedef45e0840cb1b67934676d0ccb
Bernadette Davis <bsd231@nyu.edu>
2019-10-08 17:35:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,11 +15,12 @@ class Application
 
       if match_value.length != 0
         resp.write match_value[0].price
-        #resp.write "#{match_value.price}"
       else
         resp.write "Item not found"
         resp.status = 400
       end
+
+      
     else
       resp.write "Route not found"
       resp.status = 404
